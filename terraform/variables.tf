43c6f59a265112912e4298701110c024b0b5260f
Arisofia <jenineferderas@hotmail.com>
2026-01-14 17:35:30 +0100

    ci(terraform): use unquoted types for Terraform variables; add infra/apply-oidc.sh helper

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "aws_region" {
   description = "AWS region"
-  type        = "string"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "account_id" {
   description = "AWS Account ID"
-  type        = "string"
+  type        = string
   default     = "510701314494"
 }
 
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket for documents"
-  type        = "string"
+  type        = string
   default     = "al-financial-documents-510701314494"
 }
 
 variable "ecr_repository_name" {
   description = "Name of the ECR repository"
-  type        = "string"
+  type        = string
   default     = "al-ocr-service"
 }
