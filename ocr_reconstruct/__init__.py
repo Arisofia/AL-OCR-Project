aa874a8d6dae24b3e9d4f44a8d40ccf791b3a943
Arisofia <jenineferderas@hotmail.com>
2026-01-16 12:51:50 +0100

    Fix: release workflow YAML structure

--- ocr_reconstruct/__init__.py
+++ ocr_reconstruct/__init__.py
@@ -1,5 +1,5 @@
 # OCR Reconstruct Module
-from .modules.pipeline import process_bytes
+from .modules.pipeline import process_bytes  # noqa: F401
 
 __version__ = "1.2.0"
 __all__ = ["process_bytes"]
